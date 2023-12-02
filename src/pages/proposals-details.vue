d4590b960c8da4e53dd5facdadcf3a8561bb012b
Hector Palencia <hrpmicarelli@gmail.com>
2023-12-02 01:37:23 -0400

    act

--- src/pages/proposals-details.vue
+++ src/pages/proposals-details.vue
@@ -433,7 +433,7 @@ export default {
             id: Number(id),
             action: "VoteApprove"
           },
-          gas: "30000000000000"
+          gas: "36000000000000"
           // attachedDeposit: "100000000000000000000"
         };
 
@@ -452,7 +452,7 @@ export default {
             id: Number(id),
             action: "VoteReject"
           },
-          gas: "30000000000000"
+          gas: "36000000000000"
           // attachedDeposit: "100000000000000000000"
         };
 
--- src/pages/proposals.vue
+++ src/pages/proposals.vue
@@ -440,7 +440,7 @@ export default {
             id: Number(id),
             action: "VoteApprove"
           },
-          gas: "30000000000000",
+          gas: "36000000000000",
           // attachedDeposit: "1000000000000000000"
         };
 
@@ -459,7 +459,7 @@ export default {
             id: Number(id),
             action: "VoteReject"
           },
-          gas: "30000000000000"
+          gas: "36000000000000"
           // attachedDeposit: "100000000000000000000"
         };
 
