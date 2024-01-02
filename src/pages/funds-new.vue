61a203a1d3f975f8a3778324123a1730b87de2a5
detextre4 <detextre4@gmail.com>
2024-01-02 19:38:37 -0400

    fixed

--- src/pages/funds-new.vue
+++ src/pages/funds-new.vue
@@ -4,7 +4,7 @@
       <aside class="d-flex flex-center flex-grow-1" style="gap: 20px;">
         <div class="flex-column">
           <span>DAO account name</span>
-          <span>Nombre de la Dap.near</span>
+          <span>{{ dao_account_name }}</span>
         </div>
 
         <v-divider
@@ -15,7 +15,7 @@
         
         <div class="flex-column">
           <span>Total Value Locked</span>
-          <span>10,350.22 USD</span>
+          <span>{{ total_value_computed }} USD</span>
         </div>
       </aside>
     </toolbar>
@@ -167,7 +167,7 @@
   </div>
 </template>
 
-<script setup>
+<script>
 import '@/assets/styles/pages/funds-new.scss'
 import near from '@/assets/sources/icons/near-icon.svg';
 import stnear from '@/assets/sources/icons/stnear-icon.svg';
@@ -175,257 +175,300 @@ import usdc from '@/assets/sources/icons/usdc-icon.svg';
 import usdt from '@/assets/sources/icons/tether-icon.svg';
 import VueApexchart from "vue3-apexcharts"
 import { ref } from 'vue';
-import { computed } from 'vue';
-import { onBeforeMount } from 'vue';
 import { useQuery } from '@vue/apollo-composable';
+import axios from 'axios';
 import gql from 'graphql-tag';
 import graphQl from '@/services/graphQl';
-const
-iconMap = { near, stnear, usdc, usdt },
-tokenCards = ref([
-  {
-    icon: 'near',
-    amount: 0,
-    currency: 'NEAR',
-    amount_usd: 0
-  },
-  {
-    icon: 'usdt',
-    amount: 0,
-    currency: 'USDT',
-    amount_usd: 0
-  },
-  /*{
-    icon: 'stnear',
-    amount: '1234.87',
-    currency: 'STNEAR',
-    amount_usd: '3456.878'
-  },
-  {
-    icon: 'usdc',
-    amount: '1234.87',
-    currency: 'USDC',
-    amount_usd: '3456.878'
-  },*/
-]),
-windowStep = ref(0),
-toggle = ref(0),
-dataToggle = ["7D", "1W", "1M", "1Y", "ALL"],
-chartHeight = computed(() => window.innerWidth < 690 ? '250px' : '450px'),
-seriesNear = ref(null),
-chartOptionsNear = ref(null),
-seriesUsdt = ref(null),
-chartOptionsUsdt = ref(null),
-select_radio_buttons = ref("todos"),
-typeTransaction = ref([
-  {id: "todos", desc: "Todos"},
-  {id: "received", desc: "Recibidos"},
-  {id: "transfer", desc: "Enviados"},
-]),
-address = ref(null),
-selectedOrderBy = ref({id: "asc", desc: "Menos Recientes"}),
-dataTransactions = ref([]),
-transactions_list = ref([]),
-currentPage = ref(1),
-totalPages = ref(Math.ceil(0 / 0)),
-cardsPerPage = ref(10),
-filter = ref('less'),
-filters = ref([
-  {
-    name: "Less Recent",
-    value: "less"
-  },
-])
 
+export default {
+  components: { VueApexchart },
+  setup() {
+    const QUERY = gql`
+      query MyQuery {
+        serie(id: "1") {
+          supply
+        }
 
-onBeforeMount(getData)
+        delegations {
+          total_amount
+          id
+        }
 
-const QUERY = gql`
-  query MyQuery {
-    serie(id: "1") {
-      supply
-    }
+        delegationhists(where: {delegation_: {id: "NEAR"}}, orderBy: date_time, orderDirection: desc) {
+          delegator
+          amount
+          date_time
+        }
 
-    delegations {
-      total_amount
-      id
-    }
-
-    delegationhists(where: {delegation_: {id: "NEAR"}}, orderBy: date_time, orderDirection: desc) {
-      delegator
-      amount
-      date_time
-    }
-
-    proposaldata(id: "1") {
-      proposal_actives
-      proposal_total
-    }
-  }
-`,
-QUERY_USDT = gql`
-  query MyQuery {
-    delegationhists(where: {delegation_: {id: "USDT"}}, orderBy: date_time, orderDirection: desc) {
-      delegator
-      amount
-      date_time
-    }
-  }
-`,
-resultChartNear = useQuery(QUERY),
-resultChartUsdt = useQuery(QUERY_USDT)
-
-function getData() {
-  if(resultChartNear.result){
-    if(resultChartNear.result.delegationhists) {
-      loadChartNear(resultChartNear.result);
-      console.log('here');
-    }
-  }
-  if(resultChartUsdt.result){
-    if(resultChartUsdt.result.delegationhists) {
-      loadChartUsdt(resultChartUsdt.result);
-    }
-  }
-  loadTransactions()
-}
-
-
-function mapChart(response, asset) {
-  const data_series = [];
-  const data_chartOptions = [];
-
-  for(let i = 0; i < response.delegationhists.length; i++){
-    const divider = asset == "NEAR" ? 1000000000000000000000000 : 1000000;
-    const amount = Number((response.delegationhists[i].amount / divider).toFixed(2))
-    const epoch = response.delegationhists[i].date_time/1000000;
-
-    data_series.push(amount);
-    data_chartOptions.push(epoch);
-  }
-
-  let series = [ { name: 'series', data: data_series } ];
-
-  let chartOptions = {
-    tooltip: {
-      theme: 'custom-tooltip',
-      custom: function({ series, seriesIndex, dataPointIndex, w }) {
-        const value = series[seriesIndex][dataPointIndex];
-
-        return '<div class="custom-tooltip-content">' +
-          '<span>' + asset + value + '</span>' +
-          '</div>';
+        proposaldata(id: "1") {
+          proposal_actives
+          proposal_total
+        }
       }
-    },
-    chart: { type: 'area', toolbar: { show:false } },
-    dataLabels: { enabled: false },
-    stroke: { curve: 'smooth', width: 1 },
-    xaxis: {
-      type: 'datetime',
-      categories: data_chartOptions,
-      labels: { style: { colors: '#fff' } },
-      tooltip: { enabled: false },
-    },
-    yaxis: {
-      labels: { style: { colors: '#fff' } },
-      axisBorder: { show: true },
-    },
-    grid: { show: false },
-  };
-
-  return { series, chartOptions };
-}
-
-function loadChartNear(response) {
-  if(response){
-    if(response.delegationhists) {
-      seriesNear.value = null;
-      chartOptionsNear.value = null;
-
-      const { series, chartOptions } = mapChart(response, 'NEAR');
-
-      seriesNear.value = series;
-      chartOptionsNear.value = chartOptions;
-    }
-  }
-}
-
-function loadChartUsdt(response) {
-  if(response){
-    if(response.delegationhists) {
-      seriesUsdt.value = null;
-      chartOptionsUsdt.value = null;
-
-      const { series, chartOptions } = mapChart(response, 'USDT');
-
-      seriesUsdt.value = series;
-      chartOptionsUsdt.value = chartOptions;
-    }
-  }
-}
-
-async function loadTransactions() {
-  let typeTransactions = typeTransaction.value.map(item => { return item.id }).splice(1, typeTransaction.value.length);
-  typeTransactions = select_radio_buttons.value != 'todos' ? [select_radio_buttons.value] : typeTransactions;
-
-  const query1 = `query MyQuery($typeTransactions: [String], $orderDirection: String) {
-    fundshists(where: {type_in: $typeTransactions}, orderBy: date_time, orderDirection: $orderDirection ) {
-      amount
-      date_time
-      id
-      token_id
-      type
-      user_id
-    }
-  }`;
-
-  const query2 = `query MyQuery($typeTransactions: [String], $address: String, $orderDirection: String) {
-    fundshists(where: {type_in: $typeTransactions, user_id_contains_nocase: $address}, orderBy: date_time, orderDirection: $orderDirection ) {
-      amount
-      date_time
-      id
-      token_id
-      type
-      user_id
-    }
-  }`;
-
-  const querys = !address.value ? query1 : query2;
-
-  const variables = { typeTransactions, address: address.value, orderDirection: selectedOrderBy.value?.id };
-
-  await graphQl.getQuery(querys, variables).then(response => {
-    loadTransactionsTable(response.data.data.fundshists);
-  });
-}
-
-
-function loadTransactionsTable(response) {
-  const dataTransactions = response.map((item) => {
-    const epoch = item.date_time/1000000;
+    `,
+    QUERY_USDT = gql`
+      query MyQuery {
+        delegationhists(where: {delegation_: {id: "USDT"}}, orderBy: date_time, orderDirection: desc) {
+          delegator
+          amount
+          date_time
+        }
+      }
+    `,
+    resultChartNear = useQuery(QUERY),
+    resultChartUsdt = useQuery(QUERY_USDT)
 
     return {
-        near: (item.type == "received" ? '+ ' : '- ')+Number(item.amount).toFixed(2)+' '+item.token_id,
-        icon: item.type == "received" ? 'mdi-tray-arrow-down' : 'mdi-tray-arrow-up',
-        name: item.user_id,
-        date: moment(epoch).format("DD MMM yyyy HH:mm:ss")
+      resultChartNear,
+      resultNear: ref(resultChartNear.result),
+      resultChartUsdt,
+      resultUsdt: ref(resultChartUsdt.result),
+			dao_account_name: process.env.CONTRACT_DAO,
+			total_value: ref(0),
+      iconMap: { near, stnear, usdc, usdt },
+      tokenCards: ref([
+        {
+          icon: 'near',
+          amount: 0,
+          currency: 'NEAR',
+          amount_usd: 0
+        },
+        {
+          icon: 'usdt',
+          amount: 0,
+          currency: 'USDT',
+          amount_usd: 0
+        },
+        /*{
+          icon: 'stnear',
+          amount: '1234.87',
+          currency: 'STNEAR',
+          amount_usd: '3456.878'
+        },
+        {
+          icon: 'usdc',
+          amount: '1234.87',
+          currency: 'USDC',
+          amount_usd: '3456.878'
+        },*/
+      ]),
+      windowStep: ref(0),
+      toggle: ref(0),
+      dataToggle: ["7D", "1W", "1M", "1Y", "ALL"],
+      seriesNear: ref(null),
+      chartOptionsNear: ref(null),
+      seriesUsdt: ref(null),
+      chartOptionsUsdt: ref(null),
+      select_radio_buttons: ref("todos"),
+      typeTransaction: ref([
+        {id: "todos", desc: "Todos"},
+        {id: "received", desc: "Recibidos"},
+        {id: "transfer", desc: "Enviados"},
+      ]),
+      address: ref(null),
+      selectedOrderBy: ref({id: "asc", desc: "Menos Recientes"}),
+      dataTransactions: ref([]),
+      transactions_list: ref([]),
+      currentPage: ref(1),
+      totalPages: ref(Math.ceil(0 / 0)),
+      cardsPerPage: ref(10),
+      filter: ref('less'),
+      filters: ref([
+        {
+          name: "Less Recent",
+          value: "less"
+        },
+      ])
+    }
+  },
+  computed: {
+    chartHeight() {
+      return window.innerWidth < 690 ? '250px' : '450px'
+    },
+    total_value_computed() {
+      if(this.resultChartNear.result) {
+        const balanceUsdt = this.resultChartNear.result?.delegations?.find(item => item.id == "USDT")?.total_amount / 1000000;
+        const balanceNear = this.resultChartNear.result?.delegations?.find(item => item.id == "NEAR")?.total_amount / 1000000000000000000000000;
+
+        this.tokenCards[0].amount = balanceNear.toFixed(5);
+        this.tokenCards[1].amount = balanceUsdt.toFixed(2);
+
+        axios.post(process.env.URL_APIP_PRICE,{fiat: "USD", crypto: "NEAR"})
+        .then((response) => {
+          // console.log("balance: ", response)
+          this.tokenCards[0].amount_usd = Number((balanceNear * response.data[0].value).toFixed(2))
+        }).catch((error) => {
+          console.log("error balane: ", error)
+        })
+
+        axios.post(process.env.URL_APIP_PRICE,{fiat: "USD", crypto: "USDT"})
+        .then((response) => {
+          this.tokenCards[1].amount_usd = Number((balanceUsdt * response.data[0].value).toFixed(2))
+        }).catch((error) => {
+          console.log("error balane: ", error)
+        })
+
+      }
+      return (this.tokenCards[0].amount_usd + this.tokenCards[1].amount_usd).toFixed(2)
+    }
+  },
+  watch: {
+    resultNear(response) {
+      this.loadChartNear(response);
+    },
+    resultUsdt(response) {
+      this.loadChartUsdt(response);
+    },
+  },
+  beforeMount() {
+    this.getData()
+  },
+  methods: {
+    getData() {
+      if(this.resultChartNear.result){
+        if(this.resultChartNear.result.delegationhists) {
+          this.loadChartNear(this.resultChartNear.result);
+          console.log('here');
+        }
+      }
+      if(this.resultChartUsdt.result){
+        if(this.resultChartUsdt.result.delegationhists) {
+          this.loadChartUsdt(this.resultChartUsdt.result);
+        }
+      }
+      this.loadTransactions()
+    },
+    mapChart(response, asset) {
+      const data_series = [];
+      const data_chartOptions = [];
+
+      for(let i = 0; i < response.delegationhists.length; i++){
+        const divider = asset == "NEAR" ? 1000000000000000000000000 : 1000000;
+        const amount = Number((response.delegationhists[i].amount / divider).toFixed(2))
+        const epoch = response.delegationhists[i].date_time/1000000;
+
+        data_series.push(amount);
+        data_chartOptions.push(epoch);
+      }
+
+      let series = [ { name: 'series', data: data_series } ];
+
+      let chartOptions = {
+        tooltip: {
+          theme: 'custom-tooltip',
+          custom: function({ series, seriesIndex, dataPointIndex, w }) {
+            const value = series[seriesIndex][dataPointIndex];
+
+            return '<div class="custom-tooltip-content">' +
+              '<span>' + asset + value + '</span>' +
+              '</div>';
+          }
+        },
+        chart: { type: 'area', toolbar: { show:false } },
+        dataLabels: { enabled: false },
+        stroke: { curve: 'smooth', width: 1 },
+        xaxis: {
+          type: 'datetime',
+          categories: data_chartOptions,
+          labels: { style: { colors: '#fff' } },
+          tooltip: { enabled: false },
+        },
+        yaxis: {
+          labels: { style: { colors: '#fff' } },
+          axisBorder: { show: true },
+        },
+        grid: { show: false },
       };
-  });
 
-  // const startIndex = (this.currentPage - 1) * this.cardsPerPage;
-  // const endIndex = startIndex + this.cardsPerPage;
+      return { series, chartOptions };
+    },
+    loadChartNear(response) {
+      if(response){
+        if(response.delegationhists) {
+          this.seriesNear.value = null;
+          this.chartOptionsNear.value = null;
 
-  totalPages.value = Math.ceil(dataTransactions.length / cardsPerPage.value);
-  transactions_list.value = dataTransactions;
-  loadPage();
-  // dataTransactions.value = dataTransactions.slice(startIndex, endIndex);
+          const { series, chartOptions } = this.mapChart(response, 'NEAR');
 
-}
+          this.seriesNear.value = series;
+          this.chartOptionsNear.value = chartOptions;
+        }
+      }
+    },
+    loadChartUsdt(response) {
+      if(response){
+        if(response.delegationhists) {
+          this.seriesUsdt.value = null;
+          this.chartOptionsUsdt.value = null;
 
-function loadPage() {
-  const startIndex = (currentPage.value - 1) * cardsPerPage.value;
-  const endIndex = startIndex + cardsPerPage.value;
+          const { series, chartOptions } = this.mapChart(response, 'USDT');
 
-  dataTransactions.value = transactions_list.value.slice(startIndex, endIndex);
+          this.seriesUsdt.value = series;
+          this.chartOptionsUsdt.value = chartOptions;
+        }
+      }
+    },
+    async loadTransactions() {
+      let typeTransactions = this.typeTransaction.map(item => { return item.id }).splice(1, this.typeTransaction.length);
+      typeTransactions = this.select_radio_buttons != 'todos' ? [this.select_radio_buttons] : typeTransactions;
+
+      const query1 = `query MyQuery($typeTransactions: [String], $orderDirection: String) {
+        fundshists(where: {type_in: $typeTransactions}, orderBy: date_time, orderDirection: $orderDirection ) {
+          amount
+          date_time
+          id
+          token_id
+          type
+          user_id
+        }
+      }`;
+
+      const query2 = `query MyQuery($typeTransactions: [String], $address: String, $orderDirection: String) {
+        fundshists(where: {type_in: $typeTransactions, user_id_contains_nocase: $address}, orderBy: date_time, orderDirection: $orderDirection ) {
+          amount
+          date_time
+          id
+          token_id
+          type
+          user_id
+        }
+      }`;
+
+      const querys = !this.address ? query1 : query2;
+
+      const variables = { typeTransactions, address: this.address, orderDirection: this.selectedOrderBy?.id };
+
+      await graphQl.getQuery(querys, variables).then(response => {
+        this.loadTransactionsTable(response.data.data.fundshists);
+      });
+    },
+    loadTransactionsTable(response) {
+      const dataTransactions = response.map((item) => {
+        const epoch = item.date_time/1000000;
+
+        return {
+            near: (item.type == "received" ? '+ ' : '- ')+Number(item.amount).toFixed(2)+' '+item.token_id,
+            icon: item.type == "received" ? 'mdi-tray-arrow-down' : 'mdi-tray-arrow-up',
+            name: item.user_id,
+            date: moment(epoch).format("DD MMM yyyy HH:mm:ss")
+          };
+      });
+
+      // const startIndex = (this.currentPage - 1) * this.cardsPerPage;
+      // const endIndex = startIndex + this.cardsPerPage;
+
+      this.totalPages = Math.ceil(dataTransactions.length / this.cardsPerPage);
+      this.transactions_list = dataTransactions;
+      this.loadPage();
+      // dataTransactions.value = dataTransactions.slice(startIndex, endIndex);
+
+    },
+    loadPage() {
+      const startIndex = (this.currentPage - 1) * this.cardsPerPage;
+      const endIndex = startIndex + this.cardsPerPage;
+
+      this.dataTransactions = this.transactions_list.slice(startIndex, endIndex);
+    }
+  }
 }
 </script>
