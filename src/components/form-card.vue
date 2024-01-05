26e0186d5522898dad36cbbb582b3bca671e4f40
detextre4 <detextre4@gmail.com>
2024-01-04 21:05:30 -0400

    finished create dao

--- src/components/form-card.vue
+++ src/components/form-card.vue
@@ -12,8 +12,8 @@
           <slot />
         </v-form>
 
-        <aside class="d-flex flex-wrap align-center justify-start mt-2" style="gap: 20px;">
-          <v-btn v-if="hasPrevEmit" class="bg-tertiary" @click="emit('prev')">
+        <aside class="d-flex flex-wrap align-center justify-start mt-2" style="column-gap: 20px; row-gap: 10px;">
+          <v-btn v-if="hasPrevEmit" class="bg-secondary" @click="emit('prev')">
             {{ prevText }}
             <v-icon icon="mdi-chevron-right" />
           </v-btn>
@@ -85,6 +85,82 @@ const onNext = () => emit('next', formValid.value)
     &-text {
       display: flex;
       flex-direction: column;
+
+      label {
+        color: #939393 !important;
+      }
+
+      input, textarea, .v-select__selection-text { color: #333 !important }
+      label, p { font-size: 14px !important }
+      input, textarea, .v-btn { font-size: 13px !important }
+      .v-select__selection-text { font-size: 17px !important }
+
+      .v-messages__message {
+        margin-block: 0 10px;
+      }
+
+      .v-input {
+        .mdi-menu-down { color: rgb(var(--v-theme-primary)) }
+
+        &.v-select .v-field {
+          &__field {
+            height: 42px;
+          }
+
+          &__input {
+            padding: 0;
+            min-height: 100% !important;
+            height: 100% !important;
+          }
+        }
+
+        &.v-autocomplete .v-field {
+          &__field {
+            height: 42px;
+          }
+
+          &__input {
+            padding: 0;
+            padding-left: 17px;
+            min-height: 100% !important;
+            height: 100% !important;
+          }
+        }
+
+        .v-field--variant-solo {
+          border-radius: 8px;
+          border: 1px solid hsla(0, 0%, 66%, 0.4);
+          box-shadow: none !important;
+        }
+
+        &:not(.v-textarea) .v-field__field {
+          height: 42px;
+
+          input {
+            min-height: 100% !important;
+            height: 100% !important;
+          }
+        }
+      }
+
+      .v-data-table {
+        $border: 1px solid rgba(0, 0, 0, .12);
+
+        th:nth-child(3) {
+          max-width: 90px;
+          border-inline: $border;
+        }
+
+        td:nth-child(3) {
+          border-inline: $border;
+        }
+
+        tr:last-child td {
+          border-bottom: $border;
+        }
+
+        &-footer { display: none !important }
+      }
     }
   }
 }
--- src/pages/create-dao.vue
+++ src/pages/create-dao.vue
@@ -33,7 +33,33 @@
               windowStep++
             }"
           >
-            here
+            <label for="name">Nombre del DAO:</label>
+            <v-text-field
+              id="name"
+              placeholder="Nombre del DAO"
+              variant="solo"
+              class="mb-1"
+              :rules="[globalRules.required]"
+            />
+
+            <label for="address">Dirección DAO (auto llenado):</label>
+            <v-autocomplete
+              id="address"
+              placeholder="prueba-de-dao.sputnikv2.testnet"
+              variant="solo"
+              class="mb-1"
+              :items="['prueba-de-dao.sputnikv2.testnet']"
+              :rules="[globalRules.required]"
+            />
+
+            <label for="proposal-dao">Propósito</label>
+            <v-textarea
+              id="proposal-dao"
+              placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis accumsan urna ac placerat. Ut scelerisque eu ligula ac rhoncus. Aliquam sagittis sapien sit amet libero ultricies varius. Curabitur ac ligula ultricies, semper ipsum nec, auctor sapien. Etiam nec sem ac mauris imperdiet rutrum. Sed mi dui, mattis vel ipsum eget, dictum interdum augue. Donec mollis congue enim quis dignissim. Ut egestas dolor at mauris suscipit dictum. Quisque at sollicitudin"
+              variant="solo"
+              class="mb-1"
+              :rules="[globalRules.required]"
+            />
           </form-card>
         </v-window-item>
 
@@ -47,7 +73,21 @@
             @prev="windowStep--"
             @next="windowStep++"
           >
-            here
+            <label for="legacy-state">Explique el estado legal y la jurisdicción de su DAO ( si se conoce) :</label>
+            <v-text-field
+              id="legacy-state"
+              placeholder="Estado legal del DAO"
+              variant="solo"
+              class="mb-1"
+            />
+
+            <label for="proposal-kyc">Propósito</label>
+            <v-text-field
+              id="proposal-kyc"
+              placeholder="https:// Documento_Legal"
+              variant="solo"
+              class="mb-1"
+            />
           </form-card>
         </v-window-item>
 
@@ -61,7 +101,31 @@
             @prev="windowStep--"
             @next="windowStep++"
           >
-            here
+            <p class="mb-8">¿Buscas hacer crecer a los miembros de DAO? Agregue enlaces para permitir que las personas aprendan más sobre su DAO. :</p>
+
+            <v-text-field
+              v-for="(item, i) in daoLinks"
+              v-model="item.model"
+              :key="i"
+              placeholder="https://"
+              class="appened"
+              variant="solo"
+            >
+              <template #append-inner>
+                <v-btn
+                  min-width="70"
+                  height="42"
+                  :color="i == daoLinks.length - 1 ? '#61C2D5' : '#505050'"
+                  style="border-radius: 8px !important;"
+                  @click="() => {
+                    if (i == daoLinks.length - 1) return daoLinks.push({ model: undefined })
+                    daoLinks.splice(i, 1)
+                  }"
+                >
+                  <v-icon :icon="i == daoLinks.length - 1 ? 'mdi-plus' : 'mdi-minus'" size="25" class="text-white" />
+                </v-btn>
+              </template>
+            </v-text-field>
           </form-card>
         </v-window-item>
 
@@ -75,7 +139,39 @@
             @prev="windowStep--"
             @next="windowStep++"
           >
-            here
+            <label for="default-groups" style="color: #333 !important;">Grupos predeterminados</label>
+            <span class="d-block mb-3" style="color: #939393 !important;">No puedes eliminarlos</span>
+            <v-text-field
+              id="default-groups"
+              placeholder="council"
+              variant="solo"
+              class="mb-1"
+            />
+
+            <p class="d-block mb-3">Grupos personalizados</p>
+            <v-text-field
+              v-for="(item, i) in customGroups"
+              v-model="item.model"
+              :key="i"
+              placeholder="Nombre del grupo"
+              class="appened"
+              variant="solo"
+            >
+              <template #append-inner>
+                <v-btn
+                  min-width="70"
+                  height="42"
+                  :color="i == customGroups.length - 1 ? '#61C2D5' : '#505050'"
+                  style="border-radius: 8px !important;"
+                  @click="() => {
+                    if (i == customGroups.length - 1) return customGroups.push({ model: undefined })
+                    customGroups.splice(i, 1)
+                  }"
+                >
+                  <v-icon :icon="i == customGroups.length - 1 ? 'mdi-plus' : 'mdi-minus'" size="25" class="text-white" />
+                </v-btn>
+              </template>
+            </v-text-field>
           </form-card>
         </v-window-item>
 
@@ -89,7 +185,35 @@
             @prev="windowStep--"
             @next="windowStep++"
           >
-            here
+            <p class="d-block mb-3">Agregue miembros a su DAO.</p>
+
+            <div v-for="(item, i) in daoMembers" :key="i" class="d-flex" style="gap: 20px;">
+              <v-text-field
+                v-model="item.member"
+                placeholder="ap6ay7auhan6a78ahah8gfcvbay77a9a0han5"
+                variant="solo"
+              />
+
+              <v-autocomplete
+                v-model="item.type"
+                placeholder="Council"
+                :items="['Council']"
+                variant="solo"
+              />
+
+              <v-btn
+                min-width="70"
+                height="42"
+                :color="i == daoMembers.length - 1 ? '#61C2D5' : '#505050'"
+                style="border-radius: 8px !important;"
+                @click="() => {
+                  if (i == daoMembers.length - 1) return daoMembers.push({ model: undefined })
+                  daoMembers.splice(i, 1)
+                }"
+              >
+                <v-icon :icon="i == daoMembers.length - 1 ? 'mdi-plus' : 'mdi-minus'" size="25" class="text-white" />
+              </v-btn>
+            </div>
           </form-card>
         </v-window-item>
 
@@ -103,7 +227,52 @@
             @prev="windowStep--"
             @next="windowStep++"
           >
-            here
+            <p class="d-block mb-10">Elija qué derechos de creación le otorga a los grupos DAO. Esto se puede cambiar en la configuración más adelante</p>
+
+            <v-data-table
+              density="compact"
+              class="mb-4"
+              :headers="[
+                { key: 'name', sortable: false },
+                { key: 'all', title: 'Todos', align: 'center', sortable: false },
+                { key: 'group', title: 'Grupo Personalizado', align: 'center', sortable: false },
+                { key: 'council', title: 'Council', align: 'center', sortable: false },
+              ]"
+              :items="proposals"
+            >
+              <template #item.all="{ item }">
+                <v-checkbox
+                  v-model="item.all"
+                  density="compact"
+                  color="rgb(var(--v-theme-tertiary))"
+                  class="text-tertiary flex-center"
+                  true-icon="mdi-checkbox-outline"
+                  hide-details
+                />
+              </template>
+
+              <template #item.group="{ item }">
+                <v-checkbox
+                  v-model="item.group"
+                  density="compact"
+                  color="rgb(var(--v-theme-tertiary))"
+                  class="text-tertiary flex-center"
+                  true-icon="mdi-checkbox-outline"
+                  hide-details
+                />
+              </template>
+
+              <template #item.council="{ item }">
+                <v-checkbox
+                  v-model="item.council"
+                  density="compact"
+                  color="rgb(var(--v-theme-tertiary))"
+                  class="text-tertiary flex-center"
+                  true-icon="mdi-checkbox-outline"
+                  hide-details
+                />
+              </template>
+            </v-data-table>
           </form-card>
         </v-window-item>
 
@@ -117,7 +286,52 @@
             @prev="windowStep--"
             @next="windowStep++"
           >
-            here
+            <p class="d-block mb-10">Elija qué derechos de voto le otorga a los grupos DAO</p>
+
+            <v-data-table
+              density="compact"
+              class="mb-4"
+              :headers="[
+                { key: 'name', sortable: false },
+                { key: 'all', title: 'Todos', align: 'center', sortable: false },
+                { key: 'group', title: 'Grupo Personalizado', align: 'center', sortable: false },
+                { key: 'council', title: 'Council', align: 'center', sortable: false },
+              ]"
+              :items="permissions"
+            >
+              <template #item.all="{ item }">
+                <v-checkbox
+                  v-model="item.all"
+                  density="compact"
+                  color="rgb(var(--v-theme-tertiary))"
+                  class="text-tertiary flex-center"
+                  true-icon="mdi-checkbox-outline"
+                  hide-details
+                />
+              </template>
+
+              <template #item.group="{ item }">
+                <v-checkbox
+                  v-model="item.group"
+                  density="compact"
+                  color="rgb(var(--v-theme-tertiary))"
+                  class="text-tertiary flex-center"
+                  true-icon="mdi-checkbox-outline"
+                  hide-details
+                />
+              </template>
+
+              <template #item.council="{ item }">
+                <v-checkbox
+                  v-model="item.council"
+                  density="compact"
+                  color="rgb(var(--v-theme-tertiary))"
+                  class="text-tertiary flex-center"
+                  true-icon="mdi-checkbox-outline"
+                  hide-details
+                />
+              </template>
+            </v-data-table>
           </form-card>
         </v-window-item>
 
@@ -128,10 +342,52 @@
             :window-step="windowStep"
             :steps="steps"
             title="Crear activos DAO"
+            next-text="Crear DAO"
             @prev="windowStep--"
-            @next="windowStep++"
+            @next="createDao"
           >
-            here
+            <p class="d-block mb-6">Coloca el logo de tu DAO es opcional. Si omite, se utilizará la imagen predeterminada</p>
+
+            <div class="flex-column-center mx-auto" style="gap: 20px;">
+              <input ref="fileRef" type="file" accept="image/*" class="d-none" @change="handleLoadFile">
+              <v-img-load
+                :src="daoLogoPreview"
+                width="110"
+                height="110"
+                rounded="100"
+                border="3px solid rgb(var(--v-theme-primary))"
+                stop-loading
+                cover
+              />
+              <v-btn
+                class="bg-tertiary"
+                @click="fileRef.click()"
+              >
+                Subir Logo
+                <v-icon icon="mdi-chevron-right" />
+              </v-btn>
+            </div>
+
+            <v-divider thickness="1" class="mt-10 mb-5" color="#939393" style="opacity: 1;" />
+
+            <aside class="flex-space-center flex-wrap" style="gap: 20px; --value-fs: 17px">
+              <div class="flex-column" style="gap: 5px;">
+                <label>Transacción</label>
+                <span style="font-size: var(--value-fs) !important;">Crear Nuevo DAO</span>
+              </div>
+
+              <div class="flex-center text-end ml-auto" style="gap: 20px;">
+                <div class="flex-column" style="gap: 5px;">
+                  <label>Costo</label>
+                  <span style="font-size: var(--value-fs) !important;">6 NEAR</span>
+                </div>
+                
+                <div class="flex-column" style="gap: 5px;">
+                  <label>TGas</label>
+                  <span style="font-size: var(--value-fs) !important;">300</span>
+                </div>
+              </div>
+            </aside>
           </form-card>
         </v-window-item>
       </v-window>
@@ -142,8 +398,15 @@
 <script setup>
 import '@/assets/styles/pages/create-dao.scss'
 import FormCard from '@/components/form-card.vue'
+import variables from '@/mixins/variables';
+import { getUrlFromFile } from '@/plugins/functions';
+import { onBeforeMount } from 'vue';
 import { ref } from 'vue';
+import { useToast } from 'vue-toastification';
 const
+  toast = useToast(),
+  { globalRules } = variables,
+
 windowStep = ref(0),
 steps = [
   "Nombre y Propósito",
@@ -154,5 +417,102 @@ steps = [
   "Creación de Propuesta",
   "Permisos de Votación",
   "Create DAO Assets"
-]
+],
+daoLinks = ref([ { model: undefined } ]),
+customGroups = ref([ { model: undefined } ]),
+daoMembers = ref([ { member: undefined, type: undefined } ]),
+proposals = ref([]),
+permissions = ref([]),
+fileRef = ref(),
+daoLogoPreview = ref(null)
+
+
+onBeforeMount(getData)
+
+function getData() {
+  const rights = [
+    {
+      name: "Cambiar configuración de DAO",
+      all: false,
+      group: true,
+      council: true
+    },
+    {
+      name: "Cambiar la política de DAO",
+      all: false,
+      group: true,
+      council: true
+    },
+    {
+      name: "Recompensa",
+      all: false,
+      group: true,
+      council: true
+    },
+    {
+      name: "Transferencia",
+      all: false,
+      group: true,
+      council: true
+    },
+    {
+      name: "Encuestas",
+      all: false,
+      group: true,
+      council: true
+    },
+    {
+      name: "Eliminar miembros",
+      all: false,
+      group: true,
+      council: true
+    },
+    {
+      name: "Agregar miembros",
+      all: false,
+      group: true,
+      council: true
+    },
+    {
+      name: "Llamada de función",
+      all: false,
+      group: true,
+      council: true
+    },
+    {
+      name: "Actualizar auto",
+      all: false,
+      group: true,
+      council: true
+    },
+    {
+      name: "Actualizar remoto",
+      all: false,
+      group: true,
+      council: true
+    },
+    {
+      name: "Establecer token de voto",
+      all: false,
+      group: true,
+      council: true
+    },
+  ]
+
+  proposals.value = rights
+  permissions.value = rights
+}
+
+function handleLoadFile(event) {
+  // this is the file provided
+  const file = event.target.files[0]
+  daoLogoPreview.value = getUrlFromFile(file)
+}
+
+function createDao(formValid) {
+  if (!formValid) return
+
+  console.log('create dao function here');
+  toast('¡Felicidades la DAO ha sido creada\n con éxito!')
+}
 </script>
